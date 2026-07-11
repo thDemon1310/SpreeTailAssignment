25e3f37a57788366f4c4cfeb4eeab8939db2f91e
Utplaksh Mishra <umishra8770@gmail.com>
2026-07-11 14:30:50 +0530

    Added routes for the page
    - linked the routes to respacketed pages on app.jsx
    - added styling for the frontend in index.css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,13 @@ import ProtectedRoute from './components/ProtectedRoute';
 import Layout from './components/Layout';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import DashboardPage from './pages/DashboardPage';
+import GroupsPage from './pages/GroupsPage';
+import ExpensesPage from './pages/ExpensesPage';
+import BalancesPage from './pages/BalancesPage';
+import SettlePage from './pages/SettlePage';
+import ImportPage from './pages/ImportPage';
+
 export default function App() {
   return (
     <AuthProvider>
@@ -21,6 +28,12 @@ export default function App() {
               </ProtectedRoute>
             }
           >
+            <Route index element={<DashboardPage />} />
+            <Route path="groups" element={<GroupsPage />} />
+            <Route path="expenses" element={<ExpensesPage />} />
+            <Route path="balances" element={<BalancesPage />} />
+            <Route path="settle" element={<SettlePage />} />
+            <Route path="import" element={<ImportPage />} />
           </Route>
 
           {/* Fallback */}
